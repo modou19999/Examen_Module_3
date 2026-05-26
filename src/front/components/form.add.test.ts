8f3488d31e26c7c64b26db6bf14cd95d89621b4d
Modou fall <serignemodoufall1@gmail.com>
2026-05-26 11:47:50 +0200

    fix: correcot price assection in form test

--- src/front/components/form.add.test.ts
+++ src/front/components/form.add.test.ts
@@ -61,7 +61,7 @@ describe('ProductForm - createFormAdd', () => {
                 id: 2,
                 name: 'Nuevo producto',
                 description: 'Una descripción',
-                price: 120,
+                price: 100,
                 hasPromo: true,
                 category: 'computer',
             }),
